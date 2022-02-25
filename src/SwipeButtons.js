ad58258ad8630b33c09e1467be7922e39e0eb2be
nguyenducphucc <jonybounce@gmail.com>
2022-02-24 18:30:32 -0600

    tinder-front

---
+++ src/SwipeButtons.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./SwipeButtons.css";
+import ReplayIcon from "@mui/icons-material/Replay";
+import CloseIcon from "@mui/icons-material/Close";
+import StarRateIcon from "@mui/icons-material/StarRate";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FlashOnIcon from "@mui/icons-material/FlashOn";
+import { IconButton } from "@mui/material";
+
+function SwipeButtons() {
+  const swiped = () => {
+    console.log("Clicked!");
+  };
+
+  return (
+    <div className="swipedButtons">
+      <IconButton onClick={swiped} className="swipeButtons__repeat">
+        <ReplayIcon fontSize="large" />
+      </IconButton>
+
+      <IconButton onClick={swiped} className="swipeButtons__left">
+        <CloseIcon fontSize="large" />
+      </IconButton>
+
+      <IconButton onClick={swiped} className="swipeButtons__star">
+        <StarRateIcon fontSize="large" />
+      </IconButton>
+
+      <IconButton onClick={swiped} className="swipeButtons__right">
+        <FavoriteIcon fontSize="large" />
+      </IconButton>
+
+      <IconButton onClick={swiped} className="swipeButtons__lightning">
+        <FlashOnIcon fontSize="large" />
+      </IconButton>
+    </div>
+  );
+}
+
+export default SwipeButtons;
